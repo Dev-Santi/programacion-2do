b8b626ef91f7a31a8e0c124b94ce61b003c15be3
santinakc <santinakcfin@gmail.com>
2023-11-11 22:02:28 -0300

    avance 11/11

--- js/clases.js
+++ js/clases.js
@@ -6,3 +6,96 @@ class Categoria {
     this.descripcion = descripcion;
   }
 }
+
+class Experiencia {
+  constructor(titulo, descripcion, precio, cantidadPersonas, categoria) {
+    this.titulo = titulo;
+    this.descripcion = descripcion;
+    this.precio = precio;
+    this.cantidadPersonas = cantidadPersonas;
+    this.categoria = categoria;
+  }
+}
+
+class Compra {
+  constructor(experiencia, comprador, email) {
+    this.experiencia = experiencia;
+    this.comprador = comprador;
+    this.email = email;
+  }
+}
+
+class Sistema {
+  constructor() {
+    this.categorias = [];
+    this.experiencias = [];
+    this.compras = [];
+  }
+
+  // Gestión de categorías
+  agregarCategoria(categoria) {
+    let estaRepetida = false;
+
+    for (let i = 0; i < this.categorias.length && !estaRepetida; i++) {
+      if (this.categorias[i].nombre === categoria.nombre) {
+        estaRepetida = true;
+      }
+    }
+
+    if (estaRepetida) {
+      alert("Error, categoría existente");
+    } else {
+      this.categorias.push(categoria);
+    }
+  }
+
+  eliminarCategoria(categoria) {
+    let tieneExperiencias = false;
+
+    for (let i = 0; i < this.experiencias.length && !tieneExperiencias; i++) {
+      if (this.experiencias[i].categoria.nombre === categoria.nombre) {
+        tieneExperiencias = true;
+      }
+    }
+
+    if (tieneExperiencias) {
+      alert("Error, existen experiencias que poseen dicha categoría.");
+    } else {
+      let indexDeCategoria = this.categorias.findIndex(function (categoriaActual) {
+        return categoriaActual.nombre === categoria.nombre;
+      });
+
+      this.categorias.splice(indexDeCategoria, 1);
+    }
+  }
+
+  // Gestión de experiencias
+  agregarExperiencia(experiencia) {
+    let estaRepetida = false;
+
+    for (let i = 0; i < this.experiencias.length && !estaRepetida; i++) {
+      if (this.experiencias[i].titulo === experiencia.titulo) {
+        estaRepetida = true;
+      }
+    }
+
+    if (estaRepetida) {
+      alert("Error, la experiencia ya existe.");
+    } else {
+      this.experiencias.push(experiencia);
+    }
+  }
+}
+
+/* 
+En la parte de Informes se debe mostrar el monto de la experiencia más cara registrada (independientemente de si tuvo
+compras o no), una lista con las experiencias más veces compradas (se muestra su título) y los datos relativos a la categoría
+seleccionada arriba a la izquierda. Los datos completos de la categoría se muestran en el título “Información detallada de
+la categoría” y se presenta una lista con todas las compras realizadas. De cada una se pone nombre, mail, fecha y hora de
+realizada.
+
+Se puede eliminar una categoría solamente si no hay experiencias de ella.
+Se puede eliminar una experiencia solamente si no hay compras de ella
+
+
+*/
--- js/funciones.js
+++ js/funciones.js
@@ -1,61 +1,128 @@
 // Funciones  - obligatorio 2do semestre 2023
 
-let listaDeCategorias = [];
+let sistema = new Sistema();
 
 window.addEventListener("load", programa);
 
 function programa() {
-  let btnAgregar = document.getElementById("idBotonAgregarCategoria");
-  btnAgregar.addEventListener("click", crearCategoria);
+  /* Categorias */
+  let btnAgregarCategoria = document.getElementById("idBotonAgregarCategoria");
+  btnAgregarCategoria.addEventListener("click", crearCategoria);
+
+  let btnEliminarCategoria = document.getElementById("idBotonBajaCategoria");
+  btnEliminarCategoria.addEventListener("click", eliminarCategoria);
+
+  /* Experiencias */
+  let btnAgregarExperiencia = document.getElementById("idBotonAltaExperiencia");
+  btnAgregarExperiencia.addEventListener("click", agregarExperiencia);
 }
 
-function actualizarCombos() {
-  let combosDeCategorias = document.getElementById(
-    "idComboCategoriasIzquierda"
-  );
-
-  let combosDeCategoriasEliminar = document.getElementById(
-    "idComboCategoriasAbajo"
-  );
-
-  combosDeCategorias.innerHTML = "";
-  combosDeCategoriasEliminar.innerHTML = "";
-
-  for (let i = 0; i < listaDeCategorias.length; i++) {
-    let option1 = document.createElement("option");
-    let option2 = document.createElement("option");
-
-    option1.value = listaDeCategorias[i].nombre;
-    option1.text = listaDeCategorias[i].nombre;
-    option2.value = listaDeCategorias[i].nombre;
-    option2.text = listaDeCategorias[i].nombre;
-
-    combosDeCategorias.appendChild(option1);
-    combosDeCategoriasEliminar.appendChild(option2);
-  }
-}
-
+/* Gestionar categorias */
 function crearCategoria() {
-  let nombre = document.getElementById("idNombreCategoria").value;
-  let descripcion = document.getElementById("idDetallesCategoria").value;
+  let formulario = document.getElementById("idFormCategoria");
+  if (formulario.reportValidity()) {
+    let nombre = document.getElementById("idNombreCategoria").value;
+    let descripcion = document.getElementById("idDetallesCategoria").value;
 
-  let categoriaNueva = new Categoria(nombre, descripcion);
-  let estaRepetida = false;
+    let categoria = new Categoria(nombre, descripcion);
 
-  for (let i = 0; i < listaDeCategorias.length && !estaRepetida; i++) {
-    if (listaDeCategorias[i].nombre === categoriaNueva.nombre) {
-      estaRepetida = true;
+    sistema.agregarCategoria(categoria);
+
+    formulario.reset();
+    actualizarCombosCategorias();
+  }
+}
+
+function eliminarCategoria() {
+  let combosDeCategoriasEliminar = document.getElementById("idComboCategoriasAbajo");
+
+  let categoria = sistema.categorias.find(function (categoriaActual) {
+    if (categoriaActual.nombre === combosDeCategoriasEliminar.value) {
+      return categoriaActual;
     }
+  });
+
+  sistema.eliminarCategoria(categoria);
+  actualizarCombosCategorias();
+}
+
+function actualizarCombosCategorias() {
+  let comboDeCategoriasFiltro = document.getElementById("idComboCategoriasIzquierda");
+  let comboDeCategoriasEliminar = document.getElementById("idComboCategoriasAbajo");
+  let comboDeCategoriasExperiencias = document.getElementById("idCategoriaExperiencia");
+
+  comboDeCategoriasFiltro.innerHTML = "";
+  comboDeCategoriasEliminar.innerHTML = "";
+  comboDeCategoriasExperiencias.innerHTML = "";
+
+  for (let i = 0; i < sistema.categorias.length; i++) {
+    let opcionParaComboFiltro = document.createElement("option");
+    let opcionParaComboEliminar;
+    let opcionParaComboExperiencias;
+
+    opcionParaComboFiltro.value = sistema.categorias[i].nombre;
+    opcionParaComboFiltro.text = sistema.categorias[i].nombre;
+
+    opcionParaComboEliminar = opcionParaComboFiltro.cloneNode(true);
+    opcionParaComboExperiencias = opcionParaComboFiltro.cloneNode(true);
+
+    comboDeCategoriasFiltro.appendChild(opcionParaComboFiltro);
+    comboDeCategoriasEliminar.appendChild(opcionParaComboEliminar);
+    comboDeCategoriasExperiencias.appendChild(opcionParaComboExperiencias);
   }
 
-  if (!estaRepetida) {
-    listaDeCategorias.push(categoriaNueva);
+  actualizarBtns();
+}
+
+/* Gestionar experiencias */
+
+function agregarExperiencia() {
+  let formulario = document.getElementById("idFormExperiencia");
+  if (formulario.reportValidity()) {
+    let titulo = document.getElementById("idTituloExperiencia").value;
+    let descripcion = document.getElementById("idDescripcionExperiencia").value;
+    let precio = document.getElementById("idPrecioExperiencia").value;
+    let cantidadPersonas = document.getElementById("idCantidadPersonasExperiencia").value;
+    let categoria = document.getElementById("idCategoriaExperiencia").value;
+
+    let experiencia = new Experiencia(titulo, descripcion, precio, cantidadPersonas, categoria);
+    console.log(experiencia);
+
+    sistema.agregarExperiencia(experiencia);
+
+    formulario.reset();
+    actualizarCombosExperiencia();
+  }
+}
+
+function actualizarCombosExperiencia() {
+  let comboExperiencia = document.getElementById("idComboBajaExperiencia");
+
+  comboExperiencia.innerHTML = "";
+
+  for (let i = 0; i < sistema.experiencias.length; i++) {
+    let opcionComboExperiencia = document.createElement("option");
+
+    opcionComboExperiencia.value = sistema.experiencias[i].titulo;
+    opcionComboExperiencia.text = sistema.experiencias[i].titulo;
+
+    comboExperiencia.appendChild(opcionComboExperiencia);
+  }
+
+  actualizarBtns();
+}
+
+/* utils */
+
+function actualizarBtns() {
+  let btnEliminarCategoria = document.getElementById("idBotonBajaCategoria");
+  let btnAgregarExperiencia = document.getElementById("idBotonAltaExperiencia");
+
+  if (sistema.categorias.length > 0) {
+    btnEliminarCategoria.disabled = false;
+    btnAgregarExperiencia.disabled = false;
   } else {
-    alert("Error, categoría existente");
+    btnEliminarCategoria.disabled = true;
+    btnAgregarExperiencia.disabled = true;
   }
-
-  document.getElementById("idNombreCategoria").value = "";
-  document.getElementById("idDetallesCategoria").value = "";
-
-  actualizarCombos();
 }
